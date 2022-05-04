8ecd320ddf989766b47939050a6345bdf8d629bd
George Gelaga-King <gb7309@gmail.com>
2022-05-04 07:38:54 +0000

    Update controllers/validatingwebhookconfiguration_controller.go
    Co-authored-by: Jose Armesto <github@armesto.net>

--- controllers/validatingwebhookconfiguration_controller.go
+++ controllers/validatingwebhookconfiguration_controller.go
@@ -45,13 +45,6 @@ type ValidatingWebhookConfigurationReconciler struct {
 //+kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=validatingwebhookconfigurations/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=validatingwebhookconfigurations/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the ValidatingWebhookConfiguration object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *ValidatingWebhookConfigurationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
